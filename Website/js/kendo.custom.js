631b8d06a245d492560d12595d2ef78d46681f3c
Rolando Cruz <pspnando@me.com>
2015-08-30 00:54:26 -0700

    Prototype of website v1

---
+++ Website/js/kendo.custom.js
@@ -0,0 +1,68 @@
+// Kendo UI theme for data visualization widgets
+// Use as theme: 'newTheme' in configuration options (or change the name)
+kendo.dataviz.ui.registerTheme('newTheme', {
+    "chart": {
+        "title": {
+            "color": "#a8a8a8"
+        },
+        "legend": {
+            "labels": {
+                "color": "#a8a8a8"
+            }
+        },
+        "chartArea": {
+            "background": "#1a1a1a"
+        },
+        "seriesDefaults": {
+            "labels": {
+                "color": "#a8a8a8"
+            }
+        },
+        "axisDefaults": {
+            "line": {
+                "color": "#ffffff"
+            },
+            "labels": {
+                "color": "#a8a8a8"
+            },
+            "minorGridLines": {
+                "color": "#ffffff"
+            },
+            "majorGridLines": {
+                "color": "#ffffff"
+            },
+            "title": {
+                "color": "#4c5356"
+            }
+        },
+        "seriesColors": [
+            "#10c4b2",
+            "#ff7663",
+            "#ffb74f",
+            "#a2df53",
+            "#1c9ec4",
+            "#ff63a5"
+        ],
+        "tooltip": {}
+    },
+    "gauge": {
+        "pointer": {
+            "color": "#10c4b2"
+        },
+        "scale": {
+            "rangePlaceholderColor": "#a8a8a8",
+            "labels": {
+                "color": "#a8a8a8"
+            },
+            "minorTicks": {
+                "color": "#a8a8a8"
+            },
+            "majorTicks": {
+                "color": "#4c5356"
+            },
+            "line": {
+                "color": "#a8a8a8"
+            }
+        }
+    }
+});
